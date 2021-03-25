3c8b791d27cb9a53a1df62407a3fa51bf58adb65
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2021-03-25 22:08:01 +0000

    Update LightMovingPistonBlock.java

--- src/main/java/amymialee/lightpistons/blocks/LightMovingPistonBlock.java
+++ src/main/java/amymialee/lightpistons/blocks/LightMovingPistonBlock.java
@@ -1,10 +1,15 @@
 package amymialee.lightpistons.blocks;
 
 import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.MovingPistonBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.state.properties.PistonType;
+import net.minecraft.tileentity.PistonTileEntity;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class LightMovingPistonBlock extends MovingPistonBlock {
     public LightMovingPistonBlock() {
@@ -12,5 +17,18 @@ public class LightMovingPistonBlock extends MovingPistonBlock {
         this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH).with(TYPE, PistonType.DEFAULT));
     }
 
+    public static TileEntity createTilePiston(BlockState state, Direction direction, boolean extending, boolean shouldHeadBeRendered) {
+        return new PistonTileEntity(state, direction, extending, shouldHeadBeRendered);
+    }
 
+    @Override
+    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
+        if (!state.matchesBlock(newState.getBlock())) {
+            TileEntity tileentity = worldIn.getTileEntity(pos);
+            if (tileentity instanceof PistonTileEntity) {
+                ((PistonTileEntity)tileentity).clearPistonTileEntity();
+            }
+
+        }
+    }
 }
